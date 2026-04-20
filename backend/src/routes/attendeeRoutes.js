a3e48bb49e7b6f3ae2a948290be5bdfb3ae4eaeb
Majed9890 <majedwaz12@gmail.com>
2026-04-20 21:59:48 +0300

    ...

--- backend/src/routes/attendeeRoutes.js
+++ backend/src/routes/attendeeRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { editProfile, updateInterests, likeEvent } = require('../controllers/attendeeController');
+const { editProfile, updateInterests, likeEvent, getProfile } = require('../controllers/attendeeController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
 // attendee routes
+router.get('/profile', verifyToken, getProfile);
 router.put('/edit-profile', verifyToken, editProfile);
 router.put('/update-interests', verifyToken, updateInterests);
 router.post('/like-event', verifyToken, likeEvent);
